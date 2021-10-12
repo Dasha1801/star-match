71bf2c2d8b290a9183d078c1f78787a72438fd48
Dasha1801 <6227968@gmail.com>
2021-10-12 21:19:16 +0300

    feat:add user interface status and description elements

--- src/playNumber/playNumber.jsx
+++ src/playNumber/playNumber.jsx
@@ -1,7 +1,13 @@
 import "./playNumber.css";
+import { colors } from "../utils";
 const PlayNumber = (props) => {
   return (
-    <button key={props.number} className="number" onClick={() => console.log(props.number)}>
+    <button
+      key={props.number}
+      className="number"
+      onClick={() => console.log(props.number)}
+      style={{ backgroundColor: colors[props.status] }}
+    >
       {props.number}
     </button>
   );
